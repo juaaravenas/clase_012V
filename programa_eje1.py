097dbd748fcc0f04f98a68726fe3a86b2657edff
juaravenas <jua.aravenas@profesor.duoc.cl>
2024-07-01 21:52:23 -0400

    Ya terminamos el programa 1

--- programa_eje1.py
+++ programa_eje1.py
@@ -31,7 +31,7 @@ def menu_principal():
         '1': ('Consultar Asistencia Actual  por  alumno', consulta_asistencia_rut),
         '2': ('Visualizar alumnos Asistencia Actual < 70%.', visualiza_asistencia_70),
         '3': ('Visualizar número alumnos con “Asistencia Actual”  < 70%  de un curso', visualiza_asistencia_curso),
-        '4': ('Generar archivo alumnos con “Asistencia Actual”  < 70%  de un curso', accion3),
+        '4': ('Generar archivo alumnos con “Asistencia Actual”  < 70%  de un curso', generar_asistencia_curso),
         '5': ('Salir', salir)
     }
 
@@ -92,8 +92,20 @@ def visualiza_asistencia_curso():
     print(f"{curso_ingreso } alumnos con asistencia < 70% : {contador_alumnos} ")
     input()
 
-def accion3():
- print('')
+
+def generar_asistencia_curso():
+    curso_ingreso = input("Ingrese el curso a  revisar ")
+    lista_alumnos = obtener_fichero_asistencia()
+    with open(r'C:\Users\cetecom\Downloads\salida.csv','w', newline='') as archivo_csv:
+        escritor_csv =csv.writer(archivo_csv, delimiter=";")
+        escritor_csv.writerow(['Curso','Nombre','Asistencia'])
+        for alumnos in lista_alumnos:
+            if alumnos['Asistencia_actual'] < 70 and curso_ingreso == alumnos['curso']:
+                lista = []
+                lista.append(alumnos['curso'])
+                lista.append(alumnos['nombre'])
+                lista.append(alumnos['Asistencia_actual'])
+                escritor_csv.writerow(lista)
 
 def salir():
     print('Saliendo')
